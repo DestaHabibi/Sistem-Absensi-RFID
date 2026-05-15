99d85adb58813dd4c4417a9d2f34e7a0cf9734be
DestaHabibi <destahabibi244@gmail.com>
2026-05-15 15:41:58 +0700

    First commit

---
+++ src/main/java/com/mycompany/sistemrfid/SistemRFID.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemrfid;
+
+/**
+ *
+ * @author lenovo cor i7
+ */
+public class SistemRFID {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
